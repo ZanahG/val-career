5cbcefc183fb59f1e97768858425949b02cb3ae2
ZanahG <dsagredo.cl@gmail.com>
2026-08-24 15:45:04 -0400

    fix region lan

--- src/data/regions.ts
+++ src/data/regions.ts
@@ -1,7 +1,7 @@
 import type {CompetitiveCircuit, PlayerRegion} from "../types/career";
 
 export const getCircuitFromRegion = (region: PlayerRegion): CompetitiveCircuit => {
-  if (region === "LATAM" || region === "Brazil" || region === "North America") return "Americas";
+  if (region==="LATAM" || region==="LATAM North" || region==="Brazil" || region==="North America") return "Americas";
   if (region === "Europe" || region === "MENA" || region === "Turkey" || region === "CIS") return "EMEA";
   if (region === "China") return "China";
   return "Pacific";
